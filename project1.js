c63683d5e796ec736cdce80012cb9434c87c8b13
blobstar <bilalc8@gmail.com>
2022-05-21 13:38:15 +0200

    made interactive map

--- project1.js
+++ project1.js
@@ -1,169 +1,172 @@
-function OnOff(id) {
-    var element = document.getElementById(id);
-    element.style.fill = 'orange';
-  }
-const numOfCountry = '';
+/* var hoverCountry = document.getElementsByClassName("countries"); // this will generate an array of elements with this class
+console.log(hoverCountry);
+hoverCountry.forEach(element => { 
+    element.addEventListener('mouseenter', () => element.style.fill = 'orange');
+}); //im trying to add an event listener to each one
+
+ */
 const stateMap = [];
 
-function OnOff(id) {
+let btns = document.getElementsByClassName("countries");
+Array.from(btns).forEach(function (i) {
+    console.log(stateMap[i.id]); 
+  i.addEventListener('mouseenter', function() {
+    i.style.fill = 'orange';
+    //console.log(i);
+    d3.selectAll("rect")
+    .filter(function(d) { return d.country == i.id; })
+    .style("fill", "orange"); 
+  });
+
+  i.addEventListener('mouseleave', function() {
+    console.log(stateMap[i.id]);  
+    if (stateMap[i.id] != true){
+        i.style.fill = 'black';
+        //console.log(i);
+        d3.selectAll("rect")
+        .filter(function(d) { return d.country == i.id; })
+        .style("fill", "black");
+    } else {i.style.fill = 'orange';}
+  });
+  i.addEventListener('click', function(){
+    const state = stateMap[i.id];
+    if (state){
+        stateMap[i.id] = false;
+        i.style.fill = 'black';   
+    }
+    else{
+        stateMap[i.id] = true;
+        i.style.fill = 'orange';
+    }
+  });
+});
+
+/* function hover(id) {
+    var element = document.getElementById(id);
+    element.style.fill = 'orange';
+    d3.selectAll("rect")
+    .filter(function(d) { return d.country == id; })
+    .style("fill", "orange"); 
+    console.log('yay');
+} */
+
+/* function OnOff(id) {
   var element = document.getElementById(id);
   const state = stateMap[id];
   if (state) {
-    stateMap[id] = false;
-    element.style.fill = 'black';        
+    stateMap[id] = true;
+    element.style.fill = 'black';
+    d3.selectAll("rect")
+    .filter(function(d) { return d.country == id; })
+    .style("fill", "black");        
   }
   else {
-    stateMap[id] = true;
-    element.style.fill = 'orange';    
+    stateMap[id] = false;
+    element.style.fill = 'orange';
+    d3.selectAll("rect")
+    .filter(function(d) { return d.country == id; })
+    .style("fill", "orange");    
   }
+} */
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-var width = 800,
-  height = 400,
-  barWidth = width / 275;
+const dataset = 
+[{
+    "pop": 59308690,
+    "country": "South Africa"
+},
+{   
+    "pop": 2351625,
+    "country": "Botswana"
+},
+{   
+    "pop": 1160164,
+    "country": "eSwatini"
+},
+{   
+    "pop": 32866268,
+    "country": "Angola"
+},
+{
+    "pop": 869595,
+    "country": "Comoros"
+},
+{   
+    "pop": 89561404,
+    "country": "DRC"
+},
+{
+    "pop": 2142252,
+    "country": "Lesotho"
+},
+{
+    "pop": 27691019,
+    "country": "Madagascar"
+},
+{
+    "pop": 19129955,
+    "country": "Malawi"
+},
+{
+    "pop": 1265740,
+    "country": "Mauritius"
+},
+{
+    "pop": 31255435,
+    "country": "Mozambique"
+},
+{   "pop": 2540916,
+    "country": "Namibia"
+},
+{
+    "pop": 98462,
+    "country": "Seychelles"
+},
+{
+    "pop": 59734213,
+    "country": "Tanzania"
+},
+{
+    "pop": 18383956,
+    "country": "Zambia"
+},
+{
+    "pop": 14862927,
+    "country": "Zimbabwe"
+}];
+    
+const w = 800;
+const h = 800;
+barHeight = 25;
 
-var svgContainer = d3
-  .select('.message')
-  .append('svg')
-  .attr('width', width + 100)
-  .attr('height', height + 60);
+let scale = d3.scaleLinear();
+scale
+.domain([10, 89561404])
+.range([5,600]);
 
 
-d3.json(
-    'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json'
-  )
-    .then((data) => {
-      svgContainer
-        .append('text')
-        .attr('transform', 'rotate(-90)')
-        .attr('x', -200)
-        .attr('y', 80)
-        .text('Gross Domestic Product');
+
+var graph = d3.select(".chart")
+.attr("width", w)
+              .attr("height", h)
+              .append("svg")
+              
+
+
+var bar = graph.selectAll("g")
+  .data(dataset.sort(function(a, b){return b.pop-a.pop}))
+  .enter().append("g")
+  .attr("width", 1000);
+  bar.append("rect")
+  .style('width', d => `${scale(d.pop)}px`)
+  .attr("height", barHeight)
+  .attr("y", (d, i) => {return i*26 })
+  bar.append("text")
   
-      svgContainer
-        .append('text')
-        .attr('x', width / 2 + 120)
-        .attr('y', height + 50)
-        .text('More Information: http://www.bea.gov/national/pdf/nipaguid.pdf')
-        .attr('class', 'info');
-  
-      var years = data.data.map(function (item) {
-        var quarter;
-        var temp = item[0].substring(5, 7);
-  
-        if (temp === '01') {
-          quarter = 'Q1';
-        } else if (temp === '04') {
-          quarter = 'Q2';
-        } else if (temp === '07') {
-          quarter = 'Q3';
-        } else if (temp === '10') {
-          quarter = 'Q4';
-        }
-  
-        return item[0].substring(0, 4) + ' ' + quarter;
-      });
-  
-      var yearsDate = data.data.map(function (item) {
-        return new Date(item[0]);
-      });
-  
-      var xMax = new Date(d3.max(yearsDate));
-      xMax.setMonth(xMax.getMonth() + 3);
-      var xScale = d3
-        .scaleTime()
-        .domain([d3.min(yearsDate), xMax])
-        .range([0, width]);
-  
-      var xAxis = d3.axisBottom().scale(xScale);
-  
-      svgContainer
-        .append('g')
-        .call(xAxis)
-        .attr('id', 'x-axis')
-        .attr('transform', 'translate(60, 400)');
-  
-      var GDP = data.data.map(function (item) {
-        return item[1];
-      });
-  
-      var scaledGDP = [];
-  
-      var gdpMax = d3.max(GDP);
-  
-      var linearScale = d3.scaleLinear().domain([0, gdpMax]).range([0, height]);
-  
-      scaledGDP = GDP.map(function (item) {
-        return linearScale(item);
-      });
-  
-      var yAxisScale = d3.scaleLinear().domain([0, gdpMax]).range([height, 0]);
-  
-      var yAxis = d3.axisLeft(yAxisScale);
-  
-      svgContainer
-        .append('g')
-        .call(yAxis)
-        .attr('id', 'y-axis')
-        .attr('transform', 'translate(60, 0)');
-  
-      d3.select('svg')
-        .selectAll('rect')
-        .data(scaledGDP)
-        .enter()
-        .append('rect')
-        .attr('data-date', function (d, i) {
-          return data.data[i][0];
-        })
-        .attr('data-gdp', function (d, i) {
-          return data.data[i][1];
-        })
-        .attr('class', 'bar')
-        .attr('x', function (d, i) {
-          return xScale(yearsDate[i]);
-        })
-        .attr('y', function (d) {
-          return height - d;
-        })
-        .attr('width', barWidth)
-        .attr('height', function (d) {
-          return d;
-        })
-        .attr('index', (d, i) => i)
-        .style('fill', '#33adff')
-        .attr('transform', 'translate(60, 0)')
-        .on('mouseover', function (event, d) {
-          // d or datum is the height of the
-          // current rect
-          var i = this.getAttribute('index');
-  
-          overlay
-            .transition()
-            .duration(0)
-            .style('height', d + 'px')
-            .style('width', barWidth + 'px')
-            .style('opacity', 0.9)
-            .style('left', i * barWidth + 0 + 'px')
-            .style('top', height - d + 'px')
-            .style('transform', 'translateX(60px)');
-          tooltip.transition().duration(200).style('opacity', 0.9);
-          tooltip
-            .html(
-              years[i] +
-                '<br>' +
-                '$' +
-                GDP[i].toFixed(1).replace(/(\d)(?=(\d{3})+\.)/g, '$1,') +
-                ' Billion'
-            )
-            .attr('data-date', data.data[i][0])
-            .style('left', i * barWidth + 30 + 'px')
-            .style('top', height - 100 + 'px')
-            .style('transform', 'translateX(60px)');
-        })
-        .on('mouseout', function () {
-          tooltip.transition().duration(200).style('opacity', 0);
-          overlay.transition().duration(200).style('opacity', 0);
-        });
-    })
-    .catch((e) => console.log(e));
-  
+  .attr("x", function(d) { return (scale(d.pop) + 3); })
+       .attr("y", (d, i) => {return (i*26) + barHeight/2})
+       .attr("dy", ".35em")
+       .text(function(d) { return d.country + ": " + numberWithCommas(d.pop); })
+       
